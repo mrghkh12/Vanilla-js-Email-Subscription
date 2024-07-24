25d63e64e2f2158b784a5a6b001e8e10092ac046
mohammad reza <mrghkh12@gmail.com>
2024-07-24 23:04:38 +0330

    feat(script.js): dynamic message inner elem & reset form

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,15 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyPWa9vieNMVoCVTwL-NgpG1abnSivTM45SUQSTXBL4NeClFET5m0oq2Y-QtiAOq1yBcg/exec'
 const form = document.forms['submit-to-google-sheet']
+const messageElem = document.querySelector('.message')
+
 
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
+    .then(response => {
+        messageElem.innerHTML = 'Thank You for Subscribing!'
+        setTimeout(()=> messageElem.innerHTML = '' , 3000)
+        form.reset()
+    })
     .catch(error => console.error('Error!', error.message))
 })
